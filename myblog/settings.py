72d73b929b35042133112f364c0bfb5ffc4c19cf
gdy <gdy@exemple.com>
2017-03-13 10:03:30 +0100

    Adding login option to the blog...

--- myblog/settings.py
+++ myblog/settings.py
@@ -115,6 +115,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Gestion du login au blog avec renvoi a la page d entree lorsque identification = succes
+LOGIN_REDIRECT_URL = '/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
